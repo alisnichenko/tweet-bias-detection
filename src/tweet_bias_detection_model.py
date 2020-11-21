d81ff8f20d3543c3f47876d03f513f205165d811
enbattle <lis18@rpi.edu>
2020-11-21 14:25:38 -0500

    Added regularization and interval learning rate reduction

--- src/tweet_bias_detection_model.py
+++ src/tweet_bias_detection_model.py
@@ -5,12 +5,14 @@ processing methods, including embeddings, vectorization, etc.
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.layers.experimental.preprocessing import (
     TextVectorization
 )
+
 
 def get_model_data(csv_path: str, val_split: int) -> tuple():
     """
@@ -33,7 +35,7 @@ def get_model_data(csv_path: str, val_split: int) -> tuple():
     # Convert columns to lists for shuffling.
     biases = tweets_df['biases'].to_list()
     tweets = tweets_df['tweets'].to_list()
-    
+
     # Shuffle the data.
     seed = 1337
     rng = np.random.RandomState(seed)
@@ -47,11 +49,12 @@ def get_model_data(csv_path: str, val_split: int) -> tuple():
     val_samples = tweets[-num_val_tweets:]
     train_labels = biases[:-num_val_tweets]
     val_labels = biases[-num_val_tweets:]
-    
+
     return (train_samples, val_samples, train_labels, val_labels)
 
+
 def get_embedding_layer(path_words: str, dim: int, tokens: int,
-    seq_len: int) -> tuple():
+                        seq_len: int) -> tuple():
     """
     Returns tf.keras.layers.Embedding layer trained on stanford's glove
     dataset. This layer will be used for the bias detection model.
@@ -67,19 +70,19 @@ def get_embedding_layer(path_words: str, dim: int, tokens: int,
         '../data/tweets_biases.csv', 0.2)
     # Create vocabulary index.
     vectorizer = TextVectorization(max_tokens=tokens,
-        output_sequence_length=seq_len)
+                                   output_sequence_length=seq_len)
     text_ds = tf.data.Dataset.from_tensor_slices(train_samples).batch(128)
     vectorizer.adapt(text_ds)
-    
+
     # Incorporate glove embeddings.
     embeddings_idx = {}
-    with open(path_words) as f:
+    with open(path_words, encoding="utf8") as f:
         for line in f:
             word, coefs = line.split(maxsplit=1)
             coefs = np.fromstring(coefs, "f", sep=" ")
             embeddings_idx[word] = coefs
     print("[INFO] Finished embeddings. Found {0} word vecs."
-        .format(len(embeddings_idx)))
+          .format(len(embeddings_idx)))
 
     # Variables for embedding matrix.
     voc = vectorizer.get_vocabulary()
@@ -113,8 +116,9 @@ def get_embedding_layer(path_words: str, dim: int, tokens: int,
     print("[INFO] Built the embedding layer ({0} dim).".format(embedding_dim))
     return (embedding_layer, vectorizer)
 
-def get_model(embedding_layer: Embedding, 
-    vectorizer: TextVectorization) -> keras.Model():
+
+def get_model(embedding_layer: Embedding,
+              vectorizer: TextVectorization) -> keras.Model():
     """
     Returns a keras model with the following architecture: (...).
     Model features embedding layer pretrained on glove dataset.
@@ -125,13 +129,13 @@ def get_model(embedding_layer: Embedding,
     """
     int_sequences_input = keras.Input(shape=(None,), dtype="int64")
     embedded_sequences = embedding_layer(int_sequences_input)
-    x = layers.Conv1D(128, 4, activation="relu")(embedded_sequences)
+    x = layers.Conv1D(512, 4, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.001))(embedded_sequences)
     x = layers.MaxPooling1D(4)(x)
-    x = layers.Conv1D(128, 4, activation="relu")(x)
+    x = layers.Conv1D(512, 4, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.001))(x)
     x = layers.MaxPooling1D(4)(x)
-    x = layers.Conv1D(128, 4, activation="relu")(x)
+    x = layers.Conv1D(512, 4, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.001))(x)
     x = layers.GlobalMaxPooling1D()(x)
-    x = layers.Dense(128, activation="relu")(x)
+    x = layers.Dense(512, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.001))(x)
     x = layers.Dropout(0.5)(x)
     preds = layers.Dense(10, activation="softmax")(x)
     model = keras.Model(int_sequences_input, preds)
@@ -139,7 +143,7 @@ def get_model(embedding_layer: Embedding,
     # Print model summary in the console.
     print("[INFO] Model summary displayed below.")
     print(model.summary())
-    
+
     # Train the model.
     train_samples, val_samples, train_labels, val_labels = get_model_data(
         '../data/tweets_biases.csv', 0.2)
@@ -148,30 +152,40 @@ def get_model(embedding_layer: Embedding,
     y_train = np.array(train_labels) / 10
     y_val = np.array(val_labels) / 10
 
+    # Reduce learning rate over time to find optimal learning rate
+    batch_size = 128
+    steps_per_epoch = len(train_samples) // batch_size
+    lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+        0.001,
+        decay_steps=steps_per_epoch * 1000,
+        decay_rate=1,
+        staircase=False)
+
     # Compile the model.
-    model.compile(loss="sparse_categorical_crossentropy", optimizer="Adam",
-        metrics=["acc"])
-    model.fit(x_train, y_train, batch_size=128, epochs=20, validation_data=
-            (x_val, y_val))
+    model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy, optimizer=tf.keras.optimizers.Adam(lr_schedule),
+                  metrics=["acc"])
+    hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=20, validation_data=
+    (x_val, y_val))
 
     # Export the model.
     string_input = keras.Input(shape=(1,), dtype="string")
     x = vectorizer(string_input)
     predictions = model(x)
     end_to_end_model = keras.Model(string_input, predictions)
-    
+
     # Sample prediction.
     prob = end_to_end_model.predict(
         [["Unbelievable move by the senate, it is impressive how quickly they ruin the country."]]
     )
-    print("Probability for given input is {0}.".format(np.argmax(prob[0])))
+    print("Probability for given input is {0}.".format(prob[0][np.argmax(prob[0])]))
     print("[INFO] Trained the model. Exporting the model.")
     end_to_end_model.save('../data/')
 
     return end_to_end_model
 
+
 if __name__ == '__main__':
     # Get the model with pretrained embedding layer with glove.
     embedding, vectorizer = get_embedding_layer('../data/glove.6B.100d.txt',
-        100, 20000, 140)
+                                                100, 20000, 140)
     model = get_model(embedding, vectorizer)
